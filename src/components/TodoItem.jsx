8644f26ca3fb307bda176bf34188aea5f4a4222e
GBalatti9 <gas.balatti@gmail.com>
2023-11-25 20:42:32 -0300

    Adding images to the delete and edit buttons

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,8 +6,8 @@ export const TodoItem = ({ item }) => {
             <li className="list-group-item d-flex align-items-center justify-content-between">
                 {item.todo}
                 <div>
-                <button className="btn btn-warning">Edit</button>
-                <button className="btn btn-danger">Delete</button>
+                <button className="btn btn-warning"><img src="../../public/baseline_edit_black_24dp.png" alt="Edit" /></button>
+                <button className="btn btn-danger"><img src="../../public/baseline_delete_black_24dp.png" alt="Delete" /></button>
                 </div>
             </li>
         </>
